d08a396ff81d6f25241417dc81e835afa5a4866e
Caroline de Campos <carolinedecampos@MacBook-de-Caroline.local>
2018-10-05 22:54:02 -0300

    Gera estrutura do projeto react #5

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+
+import 'antd/dist/antd.css'
+
+import Auth from './auth/Auth'
+import PageNotFound from './components/PageNotFound'
+
+const client = new ApolloClient({
+  uri: 'http://192.168.0.121:4000'
+})
+
+function RedirectToAuth() {
+  return <Redirect to="/auth" />
+}
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Switch>
+            {/* <Route path="/admin" component={Admin} /> */}
+            <Route path="/auth" component={Auth} />
+            <Route path="/" exact={true} component={RedirectToAuth} />
+            <Route component={PageNotFound} />
+          </Switch>
+        </BrowserRouter>
+      </ApolloProvider>
+    )
+  }
+}
+
+export default App
