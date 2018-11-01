8f09213832995a226fd52f28d5e66be5718fe321
Caroline Ferrari <carolinee.decampos@gmail.com>
2018-11-01 01:26:25 -0300

    Cria prefixo e sufixo ao input

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,11 +1,22 @@
 import React from 'react'
 
+import moment from 'moment'
+
 import Input from 'antd/lib/input'
 import TextArea from 'antd/lib/input/TextArea'
-import InputNumber from 'antd/lib/input-number'
 import FormItem from 'antd/lib/form/FormItem'
+import DatePicker from 'antd/lib/date-picker'
+import InputNumber from 'antd/lib/input-number'
 
-function FormInput({ input, meta, label, type = 'text', ...rest }) {
+function FormInput({
+  input: { value, ...input },
+  meta,
+  label,
+  type = 'text',
+  prefix = '',
+  suffix = '',
+  ...rest
+}) {
   return (
     <FormItem
       label={label}
@@ -14,11 +25,25 @@ function FormInput({ input, meta, label, type = 'text', ...rest }) {
       help={meta.touched && meta.invalid ? meta.error : ''}
     >
       {type === 'textarea' ? (
-        <TextArea {...rest} {...input} />
+        <TextArea {...rest} {...input} value={value} />
       ) : type === 'number' ? (
-        <InputNumber {...rest} {...input} />
+        <InputNumber {...rest} {...input} value={value} />
+      ) : type === 'datepicker' ? (
+        <DatePicker
+          {...rest}
+          {...input}
+          value={value && moment(value)}
+          format="DD/MM/YYYY"
+        />
       ) : (
-        <Input type={type} {...rest} {...input} />
+        <Input
+          type={type}
+          {...rest}
+          {...input}
+          value={value}
+          prefix={prefix}
+          suffix={suffix}
+        />
       )}
     </FormItem>
   )
