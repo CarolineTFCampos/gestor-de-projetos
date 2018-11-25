eb771b288461a25a9a088584478864eac6ccbd34
Caroline Ferrari <carolinee.decampos@gmail.com>
2018-11-24 23:48:25 -0200

    Adiciona rotas para pagina de projetos

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
+
+import AdminLayout from '../components/AdminLayout'
 import PageNotFound from '../components/PageNotFound'
 
 import Home from './Home'
 import Roles from './Roles/Roles'
-import Contributors from './Contributors/Contributors'
+import Project from './Project/Project'
 import Projects from './Projects/Projects'
+import Contributors from './Contributors/Contributors'
 
 function RedirectToHome() {
   return <Redirect to="/admin/home" />
@@ -13,14 +16,17 @@ function RedirectToHome() {
 
 function Admin() {
   return (
-    <Switch>
-      <Route path="/admin/home" exact={true} component={Home} />
-      <Route path="/admin/roles" component={Roles} />
-      <Route path="/admin/contributors" component={Contributors} />
-      <Route path="/admin/projects" component={Projects} />
-      <Route path="/admin" exact={true} component={RedirectToHome} />
-      <Route component={PageNotFound} />
-    </Switch>
+    <AdminLayout hasFooter={true}>
+      <Switch>
+        <Route path="/admin/home" exact={true} component={Home} />
+        <Route path="/admin/roles" component={Roles} />
+        <Route path="/admin/projects" component={Projects} />
+        <Route path="/admin/project/:id" component={Project} />
+        <Route path="/admin/contributors" component={Contributors} />
+        <Route path="/admin" exact={true} component={RedirectToHome} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </AdminLayout>
   )
 }
 
