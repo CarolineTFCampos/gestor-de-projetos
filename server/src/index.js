8e23ddbaa2926cc81f31367d7eeb645f5f295c98
Caroline Ferrari <carolinee.decampos@gmail.com>
2018-11-24 23:42:09 -0200

    Adiciona api do teamwork ao graphql

--- server/src/index.js
+++ server/src/index.js
@@ -3,18 +3,26 @@ const { GraphQLServer } = require('graphql-yoga')
 
 const resolvers = require('./resolvers')
 
+const tw = require('./teamwork')
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql', // the auto-generated GraphQL schema of the Prisma API
+  endpoint: process.env.PRISMA_ENDPOINT // the endpoint of the Prisma API (value set in `.env`)
+  // debug: true // log all GraphQL queries & mutations sent to the Prisma API
+  // secret: process.env.PRISMA_SECRET, // only needed if specified in `database/prisma.yml` (value set in `.env`)
+})
+
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  resolverValidationOptions: {
+    requireResolversForResolveType: false
+  },
   context: function(req) {
     return {
       ...req,
-      db: new Prisma({
-        typeDefs: 'src/generated/prisma.graphql', // the auto-generated GraphQL schema of the Prisma API
-        endpoint: process.env.PRISMA_ENDPOINT, // the endpoint of the Prisma API (value set in `.env`)
-        debug: true // log all GraphQL queries & mutations sent to the Prisma API
-        // secret: process.env.PRISMA_SECRET, // only needed if specified in `database/prisma.yml` (value set in `.env`)
-      })
+      tw,
+      db
     }
   }
 })
