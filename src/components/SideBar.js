e21511b7e4c1822721c3f7e3399d9cda32dec669
Caroline Ferrari <carolinee.decampos@gmail.com>
2018-11-24 23:53:08 -0200

    Ajusta menu lateral para trazer lista de projetos

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 
 import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router'
+
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
 
 import Icon from 'antd/lib/icon'
 import { Sider } from 'antd/lib/layout'
@@ -21,98 +25,85 @@ const style = {
 }
 
 class SideBar extends Component {
-  constructor(props) {
-    // Passa as props para a classe pai Component
-    super(props)
+  render() {
+    let activeMenu = this.props.location.pathname.split('/')[2]
+    const activeSubmenu = ['roles', 'contributors', 'projects'].includes(
+      activeMenu
+    )
+      ? 'cruds'
+      : ''
 
-    // Inicia estado do componente
-    this.state = {
-      // Responsavel por não exibir loader no botao de login ao iniciar a aplicaçao
-      openKey: ['3']
+    if (activeMenu === 'project') {
+      activeMenu = `${activeMenu}-${this.props.location.pathname.split('/')[3]}`
     }
 
-    this.handleOpenChange = this.handleOpenChange.bind(this)
-  }
-
-  //Abre um submenu ao clicar nela
-  handleOpenChange(props) {
-    console.log('BREAKPOINT: ' + props)
-  }
-
-  render() {
     return (
       <Sider
         trigger={null}
         collapsible={true}
         style={style.sider}
         collapsed={this.props.collapsed}
-        breakpoint="lg"
-        onBreakpoint={this.handleOpenChange(this)}
       >
         {/* LOGO */}
         <div>
           <a href="/">
-            <img style={style.logo} src="/images/logo.png" alt="projects" />
+            <img
+              style={style.logo}
+              src="/images/logo.png"
+              alt="Gestor de Projetos"
+            />
           </a>
         </div>
 
         <Menu
           mode="inline"
-          onOpenChange={this.handleOpenChange}
-          defaultSelectedKeys={['1']}
+          defaultOpenKeys={[activeSubmenu]}
+          defaultSelectedKeys={[activeMenu]}
         >
-          <ItemGroup title="Cadastros">
-            <MenuItem key="1">
-              <Link to="/admin/home">
-                <Icon type="dashboard" theme="outlined" />
-                <span>Home</span>
+          <MenuItem key="home">
+            <Link to="/admin/home">
+              <Icon type="dashboard" theme="outlined" />
+              <span>Dashboard</span>
+            </Link>
+          </MenuItem>
+
+          <SubMenu
+            key="cruds"
+            title={
+              <span>
+                <Icon type="plus-circle" />
+                <span>Cadastros</span>
+              </span>
+            }
+          >
+            <MenuItem key="roles">
+              <Link to="/admin/roles">
+                <Icon type="solution" theme="outlined" />
+                <span>Papéis</span>
               </Link>
             </MenuItem>
+            <MenuItem key="contributors">
+              <Link to="/admin/contributors">
+                <Icon type="team" theme="outlined" />
+                <span>Colaboradores</span>
+              </Link>
+            </MenuItem>
+            <MenuItem key="projects">
+              <Link to="/admin/projects">
+                <Icon type="project" theme="outlined" />
+                <span>Projetos</span>
+              </Link>
+            </MenuItem>
+          </SubMenu>
 
-            <SubMenu
-              key="3"
-              title={
-                <span>
-                  <Icon type="plus-circle" />
-                  <span>Cadastros</span>
-                </span>
-              }
-            >
-              <MenuItem key="4">
-                <Link to="/admin/roles">
-                  <Icon type="solution" theme="outlined" />
-                  <span>Papéis</span>
-                </Link>
-              </MenuItem>
-              <MenuItem key="5">
-                <Link to="/admin/contributors">
-                  <Icon type="team" theme="outlined" />
-                  <span>Colaboradores</span>
-                </Link>
-              </MenuItem>
-              <MenuItem key="6">
-                <Link to="/admin/projects">
-                  <Icon type="project" theme="outlined" />
-                  <span>Projetos</span>
-                </Link>
-              </MenuItem>
-            </SubMenu>
-            <SubMenu
-              key="8"
-              title={
-                <span>
-                  <Icon type="project" />
-                  <span>Projetos</span>
-                </span>
-              }
-            >
-              <MenuItem key="9">Nome Projeto 1</MenuItem>
-              <MenuItem key="10">Nome Projeto 2</MenuItem>
-            </SubMenu>
-          </ItemGroup>
           <ItemGroup title="Projetos">
-            <MenuItem key="9">Nome Projeto 1</MenuItem>
-            <MenuItem key="10">Nome Projeto 2</MenuItem>
+            {(this.props.data.projects || []).map(function(item) {
+              return (
+                <MenuItem key={`project-${item.id}`}>
+                  <Link to={`/admin/project/${item.id}`}>{item.name}</Link>
+                </MenuItem>
+              )
+            })}
           </ItemGroup>
         </Menu>
       </Sider>
@@ -120,4 +111,15 @@ class SideBar extends Component {
   }
 }
 
-export default SideBar
+const GET_PROJECTS = gql`
+  query GetProjects {
+    projects {
+      id
+      name
+    }
+  }
+`
+
+const withGraphql = graphql(GET_PROJECTS)
+
+export default withGraphql(withRouter(SideBar))
