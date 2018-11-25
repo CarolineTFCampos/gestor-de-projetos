2c8a238f263b52f4e960e6be8a7d3d74bf187558
Caroline Ferrari <carolinee.decampos@gmail.com>
2018-11-24 23:57:03 -0200

    Adiciona suporte a select e radio button no componente de input

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -3,20 +3,26 @@ import React from 'react'
 import moment from 'moment'
 
 import Input from 'antd/lib/input'
-import TextArea from 'antd/lib/input/TextArea'
+import Radio from 'antd/lib/radio'
+import Select from 'antd/lib/select'
 import FormItem from 'antd/lib/form/FormItem'
 import DatePicker from 'antd/lib/date-picker'
+import TextArea from 'antd/lib/input/TextArea'
 import InputNumber from 'antd/lib/input-number'
 
 function FormInput({
-  input: { value, ...input },
+  input: { value, onChange, ...input },
   meta,
   label,
   type = 'text',
   prefix = '',
   suffix = '',
+  style = {},
+  children,
   ...rest
 }) {
+  style.width = style.width || '100%'
+
   return (
     <FormItem
       label={label}
@@ -25,21 +31,58 @@ function FormInput({
       help={meta.touched && meta.invalid ? meta.error : ''}
     >
       {type === 'textarea' ? (
-        <TextArea {...rest} {...input} value={value} />
+        <TextArea
+          {...rest}
+          {...input}
+          onChange={onChange}
+          style={style}
+          value={value}
+        />
       ) : type === 'number' ? (
-        <InputNumber {...rest} {...input} value={value} />
+        <InputNumber
+          {...rest}
+          {...input}
+          onChange={onChange}
+          style={style}
+          value={value}
+        />
       ) : type === 'datepicker' ? (
         <DatePicker
           {...rest}
           {...input}
-          value={value && moment(value)}
+          onChange={onChange}
+          style={style}
           format="DD/MM/YYYY"
+          value={value && moment(value)}
         />
-      ) : (
-        <Input
-          type={type}
+      ) : type === 'select' ? (
+        <Select
           {...rest}
           {...input}
+          onChange={onChange}
+          style={style}
+          value={value}
+        >
+          {children}
+        </Select>
+      ) : type === 'radiobutton' ? (
+        <Radio.Group
+          {...rest}
+          {...input}
+          onChange={event => onChange(event.target.value)}
+          style={style}
+          value={value}
+          buttonStyle="solid"
+        >
+          {children}
+        </Radio.Group>
+      ) : (
+        <Input
+          {...rest}
+          {...input}
+          onChange={onChange}
+          type={type}
+          style={style}
           value={value}
           prefix={prefix}
           suffix={suffix}
