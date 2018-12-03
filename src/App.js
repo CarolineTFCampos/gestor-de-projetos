b8cb504012458ee072f400be72af53b72bffa5b7
Caroline Ferrari <carolinee.decampos@gmail.com>
2018-12-03 12:07:22 -0200

    adiciona retorno de erro caso nao encontre o usuario autenticado

--- src/App.js
+++ src/App.js
@@ -16,9 +16,24 @@ import PageNotFound from './components/PageNotFound'
 moment.locale('pt-br')
 
 const client = new ApolloClient({
-  // uri: 'http://localhost:4000' // local
-  uri: 'http://10.0.0.190:4000' // ip
-  // uri: 'https://us1.prisma.sh/carolineedecampos-881f88/gestor-de-projetos/dev' // server
+  // uri: 'http://localhost:4000', // local
+  uri: 'http://192.168.1.12:4000', // ip
+  // uri: 'https://us1.prisma.sh/carolineedecampos-881f88/gestor-de-projetos/dev', // server
+  request: function(operation) {
+    const token = localStorage.getItem('gestor__token')
+
+    operation.setContext({
+      headers: {
+        Authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  onError: function(err) {
+    if (err.graphQLErrors[0].message === 'Não autorizado') {
+      localStorage.removeItem('gestor__token')
+      window.location = '/auth'
+    }
+  }
 })
 
 function RedirectToAuth() {
