7931cf214d334a6e083936ca93ea9ea39251634f
Caroline Ferrari <carolinee.decampos@gmail.com>
2018-12-03 12:08:39 -0200

    remove pagina home

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -4,25 +4,33 @@ import { Switch, Route, Redirect } from 'react-router-dom'
 import AdminLayout from '../components/AdminLayout'
 import PageNotFound from '../components/PageNotFound'
 
-import Home from './Home'
 import Roles from './Roles/Roles'
 import Project from './Project/Project'
 import Projects from './Projects/Projects'
+import Allocation from './Allocation/Allocation'
 import Contributors from './Contributors/Contributors'
 
 function RedirectToHome() {
-  return <Redirect to="/admin/home" />
+  return <Redirect to="/admin/projects" />
+}
+
+function RedirectToAuth() {
+  return <Redirect to="/auth" />
 }
 
 function Admin() {
+  if (!localStorage.getItem('gestor__token')) {
+    return <RedirectToAuth />
+  }
+
   return (
     <AdminLayout hasFooter={true}>
       <Switch>
-        <Route path="/admin/home" exact={true} component={Home} />
         <Route path="/admin/roles" component={Roles} />
         <Route path="/admin/projects" component={Projects} />
         <Route path="/admin/project/:id" component={Project} />
-        <Route path="/admin/contributors" component={Contributors} />
+        <Route path="/admin/contributors/crud" component={Contributors} />
+        <Route path="/admin/contributors/allocation" component={Allocation} />
         <Route path="/admin" exact={true} component={RedirectToHome} />
         <Route component={PageNotFound} />
       </Switch>
